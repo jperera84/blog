154f9b77faee54f806b8ddd71d780efab3527715
Jose R. Perera Morales <jose@Joses-iMac.local>
2017-03-23 20:38:39 -0400

    Fixing Second Review

--- handlers/commenthandler.py
+++ handlers/commenthandler.py
@@ -15,17 +15,21 @@ class CommentHandler(Handler):
         logout_text = None
         if user_url:
             user = ndb.Key(urlsafe=user_url).get()
+            user_id = ndb.Key(urlsafe=user_url).id()
             login_text = "Welcome %s" % user.username
             logout_text = "Logout"
             comment_edit_key_url = self.request.get("comment")
             entry_key_url = self.request.get("entry")
             comment_key = ndb.Key(urlsafe=comment_edit_key_url)
             comment = comment_key.get()
-            if comment:
-                self.render("comment.html", login=login_text,
-                            logout=logout_text, entry_key=entry_key_url,
-                            comment_key=comment_edit_key_url,
-                            comment=comment.content)
+            if comment.user_id == user_id:
+                if comment:
+                    self.render("comment.html", login=login_text,
+                                logout=logout_text, entry_key=entry_key_url,
+                                comment_key=comment_edit_key_url,
+                                comment=comment.content)
+                else:
+                    self.redirect("/blog/entry?entry=%s" % entry_key_url)
             else:
                 self.redirect("/blog/entry?entry=%s" % entry_key_url)
         else:
@@ -35,16 +39,21 @@ class CommentHandler(Handler):
         """ Post method to handle modify comments """
         user_url = self.request.cookies.get('user_key', None)
         if user_url:
+            user_id = ndb.Key(urlsafe=user_url).id()
             comment_edit_key_url = self.request.get("comment_key")
             comment_key = ndb.Key(urlsafe=comment_edit_key_url)
             entry_key_url = self.request.get("entry_key")
             comment = comment_key.get()
             if comment:
-                content = self.request.get('content')
-                comment.content = content
-                comment.put()
-                time.sleep(0.1)
-                self.redirect("/blog/entry?entry=%s" % entry_key_url)
+                if comment.user_id == user_id:
+                    content = self.request.get('content')
+                    comment.content = content
+                    comment.put()
+                    time.sleep(0.1)
+                    self.redirect("/blog/entry?entry=%s" % entry_key_url)
+                else:
+                    self.error(404)
+                    return
             else:
                 self.error(404)
                 return
--- handlers/entryview.py
+++ handlers/entryview.py
@@ -1,4 +1,4 @@
-""" Comment Handler """
+""" EntryView Handler """
 import time
 import webapp2
 import jinja2
@@ -13,11 +13,14 @@ class EntryView(Handler):
         """ Default get method"""
         entry_key = self.request.get("entry")
         entry = ndb.Key(urlsafe=entry_key).get()
-        user_url = self.request.cookies.get('user_key', None)
-        login_text = "Log In"
-        logout_text = None
-        if user_url:
-            user = ndb.Key(urlsafe=user_url).get()
-            login_text = "Welcome %s" % user.username
-            logout_text = "Logout"
-        self.render("entry.html", entry=entry, login=login_text, logout=logout_text)
+        if entry:
+            user_url = self.request.cookies.get('user_key', None)
+            login_text = "Log In"
+            logout_text = None
+            if user_url:
+                user = ndb.Key(urlsafe=user_url).get()
+                login_text = "Welcome %s" % user.username
+                logout_text = "Logout"
+            self.render("entry.html", entry=entry, login=login_text, logout=logout_text)
+        else:
+            self.redirect("/blog")
